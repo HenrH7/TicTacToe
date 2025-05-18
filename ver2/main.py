abe4543ccb72d739cd673da8b5069fd3b9cedbbe
HenrH7 <158587906+HenrH7@users.noreply.github.com>
2025-05-18 16:49:41 -0400

    Delete ver2 directory

--- ver2/main.py
+++
@@ -1,9 +0,0 @@
-from board import Board
-
-#1 for x 2 for o
-def main():
-    game = Board()
-    game.start()
-
-if __name__ == '__main__':  
-    main()
--- ver2/wordgenerator.py
+++
@@ -1,6 +0,0 @@
-import itertools
-
-chrs = 'abcdefghijklmnopqrstuvwxyz'
-n = 5
-for xs in itertools.product(chrs, repeat=n):
-    print(''.join(xs))
